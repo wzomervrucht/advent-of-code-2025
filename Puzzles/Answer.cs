bced812befc0ab438f5b650d500754f3b06b9563
Wouter Zomervrucht <149009213+wzomervrucht@users.noreply.github.com>
2025-12-06 23:18:42 +0100

    answer type

---
+++ Puzzles/Answer.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2025.Puzzles;
+
+public readonly record struct Answer(string Value)
+{
+    public override string ToString() => Value;
+
+    public static implicit operator Answer(string value) => new(value);
+    public static implicit operator Answer(int value) => new(value.ToString());
+    public static implicit operator Answer(long value) => new(value.ToString());
+}
--- Puzzles/Day01/Puzzle01.cs
+++ Puzzles/Day01/Puzzle01.cs
@@ -5,7 +5,7 @@ public static class Puzzle01
     public const int Day = 1;
     public const string Title = "Puzzle 1";
 
-    public static string Solve1(IReadOnlyList<string> input) => "Not implemented";
+    public static Answer Solve1(IReadOnlyList<string> input) => "Not implemented";
 
-    public static string Solve2(IReadOnlyList<string> input) => "Not implemented";
+    public static Answer Solve2(IReadOnlyList<string> input) => "Not implemented";
 }
